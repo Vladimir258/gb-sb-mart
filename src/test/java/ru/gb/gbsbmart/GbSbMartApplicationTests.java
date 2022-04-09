806fa2df6d093ea4b877a477398782ebcc575f55
Vladimir <fominsky_v@mail.ru>
2022-04-10 03:03:07 +0500

    Init projects

---
+++ src/test/java/ru/gb/gbsbmart/GbSbMartApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.gbsbmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbSbMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
